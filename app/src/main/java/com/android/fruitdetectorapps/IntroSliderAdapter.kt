6c3d460279dfd48d58977cc407397c8bcacf4346
ALU-syntax <ardianiqbal40@gmail.com>
2022-09-08 20:08:06 +0700

    menambahkan intro section

---
+++ app/src/main/java/com/android/fruitdetectorapps/IntroSliderAdapter.kt
@@ -0,0 +1,13 @@
+package com.android.fruitdetectorapps
+
+
+
+/**
+ * Created by Ardian Iqbal Yusmartito on 08/09/22
+ * Github : https://github.com/ALU-syntax
+ * Twitter : https://twitter.com/mengkerebe
+ * Instagram : https://www.instagram.com/ardian_iqbal_
+ * LinkedIn : https://www.linkedin.com/in/ardianiqbal
+ */
+class IntroSliderAdapter {
+}
